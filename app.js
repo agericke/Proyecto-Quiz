fe5af0bc0a1404d423cd6c4eb7f2dafce06a8f37
agericke <alvgericke@gmail.com>
2016-04-26 10:53:46 +0000

    Primera página y favicon

--- app.js
+++ app.js
@@ -6,26 +6,27 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
+//var users = require('./routes/users');
 
-var app = express();
+var app = express(); //Crea aplicacion express
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));//Instalaparametro de views, se le pasa en ruta el views como directorio. join es un metodo del modulo path que asocia el directorio de quiz con el directorio views y crea el path
+app.set('view engine', 'ejs');//Proque hemos puesto ejs al generar al proyecto. ejs es el renderizador utilizado por Ruby
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(favicon(__dirname + '/public/favicon.ico')); //_dirname representa el directorio absoluto del sistema (donde est'a app.js (directorio quiz en nuestro caso))
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+app.use('/', routes);//Intslar enrutadores. El index en la ruta base
+//app.use('/users', users);//El segundo lo instalamos en la ruta users
 
 // catch 404 and forward to error handler
+//Va a estar asociadas a todo el resto de rutas que no sean las otras dos. Se ejecutara esto siempre que no sea una de las rutas definidas arriba
 app.use(function(req, res, next) {
     var err = new Error('Not Found');
     err.status = 404;
@@ -36,12 +37,12 @@ app.use(function(req, res, next) {
 
 // development error handler
 // will print stacktrace
-if (app.get('env') === 'development') {
+if (app.get('env') === 'development') {//parametro env de la aplicacion sea development (estamos en dfase esarrolo)
     app.use(function(err, req, res, next) {
         res.status(err.status || 500);
         res.render('error', {
             message: err.message,
-            error: err
+            error: err //print err
         });
     });
 }
@@ -52,7 +53,7 @@ app.use(function(err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
         message: err.message,
-        error: {}
+        error: {}//Stack de errores esta vacio
     });
 });
 
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 var express = require('express');
-var router = express.Router();
+var router = express.Router();//Generamos enrutador
 
 /* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+router.get('/', function(req, res) {//Metemos el get al pth vacio, raiz.
+  res.render('index', { title: 'Quiz' });
 });
 
 module.exports = router;
