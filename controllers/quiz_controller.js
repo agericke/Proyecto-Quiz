85e8ea3e13e86ad859fdc9a98d827c5b5cb67cdd
agericke <alvgericke@gmail.com>
2016-05-05 01:13:40 +0000

    Bases de datos

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,15 +1,21 @@
-//GET /quizes/question
-exports.question = function(req,res) {
-    res.render('quizes/question', {pregunta: 'Capital de Italia', title: 'Intenta responder'});
+var models = require('../models/models.js');
+
+// GET /quizes/question
+exports.question = function(req, res) {
+  models.Quiz.findAll().success(function(quiz) {
+    res.render('quizes/question', { pregunta: quiz[0].pregunta, title: 'Intenta responder'});
+  })
 };
 
-//Get /quizes/answer
+// GET /quizes/answer
 exports.answer = function(req, res) {
-    if (req.query.respuesta === 'Roma'){
-        res.render('quizes/answer', {respuesta: 'Correcto', title: 'Respuesta buena'});
+  models.Quiz.findAll().success(function(quiz) {
+    if (req.query.respuesta === quiz[0].respuesta) {
+      res.render('quizes/answer', { respuesta: 'Correcto', title: 'Repuesta Buena'});
     } else {
-        res.render('quizes/answer', {respuesta: 'Incorrecta', title: 'Respuesta mala'});
+      res.render('quizes/answer', { respuesta: 'Incorrecto', title: 'Respuesta mala'});
     }
+  })
 };
 
 //Get /author
