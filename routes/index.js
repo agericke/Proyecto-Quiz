e5616131546a9611c000fbf07ae3273dd08a96dc
agericke <alvgericke@gmail.com>
2016-05-29 10:57:38 +0000

    Borrar preguntas

--- routes/index.js
+++ routes/index.js
@@ -29,6 +29,8 @@ router.get('/search', quizController.search);
 router.get('/quizzes/:quizId(\\d+)/edit', quizController.edit);
 router.put('/quizzes/:quizId(\\d+)', quizController.update);
 
+router.delete('/quizzes/:quizId(\\d+)', quizController.destroy);
+
 router.get('/author', quizController.autor);
 
 module.exports = router;
