67ce0630eb80e3069efe992eec56251760dc99c8
agericke <alvgericke@gmail.com>
2016-05-31 13:53:14 +0000

    Autorización

--- controllers/session_controller.js
+++ controllers/session_controller.js
@@ -1,15 +1,34 @@
 var userController = require('./user_controller');
 
+// Middleware: Se requiere hacer login.
+//
+// Si el usuario ya hizo login anteriormente entonces existira 
+// el objeto user en req.session, por lo que continuo con los demas 
+// middlewares o rutas.
+// Si no existe req.session.user, entonces es que aun no he hecho 
+// login, por lo que me redireccionan a una pantalla de login. 
+// Guardo en redir cual es mi url para volver automaticamente a 
+// esa url despues de hacer login; pero si redir ya existe entonces
+// conservo su valor.
+// 
+exports.loginRequired = function (req, res, next) {
+    if (req.session.user) {
+        next();
+    } else {
+        res.redirect('/session?redir=' + (req.param('redir') || req.url));
+    }
+};
 
 // GET /session   -- Formulario de login
 exports.new = function(req, res, next) {
-    res.render('session/new', {title:"Log in"});
+    res.render('session/new', {redir: req.query.redir || '/', title:"Log in"});
 };
 
 
 // POST /session   -- Crear la sesion si usuario se autentica
 exports.create = function(req, res, next) {
-
+    
+    var redir = req.body.redir || '/';
     var login     = req.body.login;
     var password  = req.body.password;
 
@@ -20,11 +39,11 @@ exports.create = function(req, res, next) {
 	        // La sesión se define por la existencia de: req.session.user
 	        req.session.user = {id:user.id, username:user.username};
 
-	        res.redirect("/"); // redirección a la raiz
+	        res.redirect(redir); // redirección a la raiz
 		})
 		.catch(function(error) {
             req.flash('error', 'Se ha producido un error: ' + error);
-            res.redirect("/session");        
+            res.redirect("/session?redir="+redir);        
     });
 };
 
